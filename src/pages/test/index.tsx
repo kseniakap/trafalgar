df0ede77b5344666021a99b8b29fc94ce94f379d
Kalabass <sevagavgav@yandex.ru>
2024-06-18 16:55:59 +0300

    feat: Подключил MUI + добавил компонент на тестовую страницу

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,12 +1,24 @@
+import { Box, Slider, Stack } from '@mui/material';
 import React from 'react';
 import styled from 'styled-components';
 
 import { PageContainer } from '~/components/page';
 
 const TestPage: React.FC = () => {
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    event.preventDefault();
+    setValue(newValue as number);
+  };
   return (
     <PageContainer>
       <Div>Test page</Div>
+      <Box sx={{ width: 200 }}>
+        <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+          <Slider aria-label="Volume" value={value} onChange={handleChange} />
+        </Stack>
+      </Box>
     </PageContainer>
   );
 };
