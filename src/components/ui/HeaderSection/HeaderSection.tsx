76f98963b3b6e209853c52dbce41700b66a6e4f7
aidagalimova <aidagalimova03@gmail.com>
2024-06-24 11:44:19 +0300

    feat(TRA-14): добавила блок "Доказательная медицина"

---
+++ src/components/ui/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,101 @@
+import Box from '@mui/material/Box';
+import styled from 'styled-components';
+
+import Illustration from '../../../assets/img/header-illustration.png';
+import { Button, ButtonSize, ButtonStyle, ButtonTextSize } from '../Button/Button';
+import useIsMobile from '~/hooks/useIsMobile';
+
+export const HeaderSection = () => {
+  const isMobile = useIsMobile();
+  return (
+    <HeaderContainer>
+      <LeftContainer>
+        <Title>
+          <span className={isMobile ? 'heading-4' : 'heading-1'}>ДОКАЗАТЕЛЬНАЯ МЕДИЦИНА ДЛЯ ВСЕХ</span>
+          <span className={isMobile ? 'body-m' : 'body-l'}>
+            Мы еще очень молоды, но достаточно амбициозны и планируем каждого случайного гостя нашего сайта превратить в
+            его постоянного пользователя.
+          </span>
+        </Title>
+        <Buttons>
+          <Button
+            text="Войти как врач"
+            style={ButtonStyle.Contained}
+            size={isMobile ? ButtonSize.Small : ButtonSize.Large}
+            textSize={isMobile ? ButtonTextSize.Small : ButtonTextSize.Large}
+          />
+          <Button
+            text="Подробнее"
+            style={ButtonStyle.Outlined}
+            size={isMobile ? ButtonSize.Small : ButtonSize.Large}
+            textSize={isMobile ? ButtonTextSize.Small : ButtonTextSize.Large}
+          />
+        </Buttons>
+      </LeftContainer>
+      <RightContainer>
+        <Image src={Illustration} alt={'header illustration'} />
+      </RightContainer>
+    </HeaderContainer>
+  );
+};
+
+const HeaderContainer = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  gap: 80px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 80px;
+  background: #f2f4f8;
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    gap: 32px;
+    padding: 48px 16px 24px 16px;
+  }
+`;
+
+const LeftContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 64px;
+  max-width: 600px;
+  padding: 80px 0;
+  @media (max-width: 1024px) {
+    gap: 48px;
+    padding: 0;
+  }
+`;
+
+const Title = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  @media (max-width: 768px) {
+    gap: 24px;
+  }
+`;
+
+const Buttons = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
+
+const RightContainer = styled(Box)`
+  align-content: center;
+  max-width: 600px;
+  padding-right: 14px;
+  @media (max-width: 1024px) {
+    padding-right: 0;
+  }
+  @media (max-width: 768px) {
+    padding-top: 20px;
+  }
+`;
+const Image = styled.img`
+  width: 100%;
+  @media (max-width: 1024px) {
+    max-width: 360px;
+  }
+`;
---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,20 @@
+import debounce from 'lodash/debounce';
+import { useLayoutEffect, useState } from 'react';
+
+const mobileSize = 768;
+const useIsMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < mobileSize);
+
+  useLayoutEffect(() => {
+    const updateSize = (): void => {
+      setIsMobile(window.innerWidth < mobileSize);
+    };
+    console.log(isMobile);
+    window.addEventListener('resize', debounce(updateSize, 250));
+    return (): void => window.removeEventListener('resize', updateSize);
+  }, []);
+
+  return isMobile;
+};
+
+export default useIsMobile;
