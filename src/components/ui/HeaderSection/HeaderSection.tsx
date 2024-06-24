657cfd1f63b72f7f3fefa0dc026566362bcb1e7b
aidagalimova <aidagalimova03@gmail.com>
2024-06-24 23:24:28 +0300

    feat(TRA-14): исправила замечания

--- src/components/ui/HeaderSection/HeaderSection.tsx
+++ src/components/ui/HeaderSection/HeaderSection.tsx
@@ -1,18 +1,17 @@
+import { styled } from '@mui/material';
 import Box from '@mui/material/Box';
-import styled from 'styled-components';
 
 import Illustration from '../../../assets/img/header-illustration.png';
 import { Button, ButtonSize, ButtonStyle, ButtonTextSize } from '../Button/Button';
-import useIsMobile from '~/hooks/useIsMobile';
+import muiTheme from '~/lib/themes/muiTheme';
 
 export const HeaderSection = () => {
-  const isMobile = useIsMobile();
   return (
     <HeaderContainer>
       <LeftContainer>
         <Title>
-          <TitleText isMobile={isMobile}>ДОКАЗАТЕЛЬНАЯ МЕДИЦИНА ДЛЯ ВСЕХ</TitleText>
-          <Description isMobile={isMobile}>
+          <TitleText>ДОКАЗАТЕЛЬНАЯ МЕДИЦИНА ДЛЯ ВСЕХ</TitleText>
+          <Description>
             Мы еще очень молоды, но достаточно амбициозны и планируем каждого случайного гостя нашего сайта превратить в
             его постоянного пользователя.
           </Description>
@@ -21,14 +20,14 @@ export const HeaderSection = () => {
           <Button
             text="Войти как врач"
             style={ButtonStyle.Contained}
-            size={isMobile ? ButtonSize.Small : ButtonSize.Large}
-            textSize={isMobile ? ButtonTextSize.Small : ButtonTextSize.Large}
+            size={muiTheme.breakpoints.down('tablet') ? ButtonSize.Small : ButtonSize.Large}
+            textSize={muiTheme.breakpoints.down('tablet') ? ButtonTextSize.Small : ButtonTextSize.Large}
           />
           <Button
             text="Подробнее"
             style={ButtonStyle.Outlined}
-            size={isMobile ? ButtonSize.Small : ButtonSize.Large}
-            textSize={isMobile ? ButtonTextSize.Small : ButtonTextSize.Large}
+            size={muiTheme.breakpoints.down('tablet') ? ButtonSize.Small : ButtonSize.Large}
+            textSize={muiTheme.breakpoints.down('tablet') ? ButtonTextSize.Small : ButtonTextSize.Large}
           />
         </Buttons>
       </LeftContainer>
@@ -39,86 +38,95 @@ export const HeaderSection = () => {
   );
 };
 
-const HeaderContainer = styled(Box)`
-  display: flex;
-  flex-direction: row;
-  gap: 80px;
-  justify-content: center;
-  width: 100%;
-  padding: 80px;
-  background: #f2f4f8;
-  @media (max-width: 1024px) {
-    flex-direction: column;
-    align-items: center;
-    gap: 32px;
-    padding: 48px 16px 24px 16px;
-  }
-  @media (min-width: 1440px) {
-    justify-content: space-around;
-  }
-`;
+const HeaderContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: theme.spacing(10),
+  justifyContent: 'center',
+  width: '100%',
+  padding: theme.spacing(10),
+  background: '#f2f4f8',
+  alignItems: 'center',
+  [theme.breakpoints.up('desktop')]: {
+    justifyContent: 'space-around',
+  },
+  [theme.breakpoints.down('iPad')]: {
+    flexDirection: 'column',
+    gap: theme.spacing(4),
+    padding: '48px 16px 24px 16px',
+  },
+}));
 
-const LeftContainer = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  gap: 64px;
-  max-width: 600px;
-  padding: 80px 0;
-  @media (max-width: 1024px) {
-    gap: 48px;
-    padding: 0;
-  }
-  @media (min-width: 1440px) {
-    max-width: none;
-  }
-`;
+const LeftContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(8),
+  maxWidth: '600px',
+  padding: '80px 0',
+  [theme.breakpoints.up('desktop')]: {
+    maxWidth: 'none',
+  },
+  [theme.breakpoints.down('iPad')]: {
+    gap: theme.spacing(6),
+    padding: '0',
+  },
+}));
 
-const Title = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  gap: 48px;
-  @media (max-width: 768px) {
-    gap: 24px;
-  }
-`;
+const Title = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(6),
+  [theme.breakpoints.down('tablet')]: {
+    gap: theme.spacing(3),
+  },
+}));
 
-const Buttons = styled(Box)`
-  display: flex;
-  flex-direction: row;
-  gap: 16px;
-`;
+const Buttons = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: theme.spacing(2),
+}));
 
-const RightContainer = styled(Box)`
-  align-content: center;
-  max-width: 600px;
-  padding-right: 14px;
-  @media (max-width: 1024px) {
-    padding-right: 0;
-  }
-  @media (max-width: 768px) {
-    padding-top: 20px;
-  }
-  @media (min-width: 1440px) {
-    max-width: none;
-  }
-`;
+const RightContainer = styled(Box)(({ theme }) => ({
+  dalignContent: 'center',
+  maxWidth: '600px',
+  paddingRight: '14px',
+  [theme.breakpoints.up('desktop')]: {
+    maxWidth: 'none',
+  },
+  [theme.breakpoints.down('iPad')]: {
+    paddingRight: '0',
+  },
+  [theme.breakpoints.down('tablet')]: {
+    paddingTop: '20px',
+  },
+}));
 
-const Image = styled.img`
-  width: 100%;
-  @media (max-width: 1024px) {
-    max-width: 360px;
-  }
-`;
-const TitleText = styled.span<{ isMobile: boolean }>`
-  font-size: ${(props) => (props.isMobile ? '24px' : '54px')};
-  line-height: ${(props) => (props.isMobile ? '26.4px' : '59.4px')};
-  font-weight: 700;
-  text-align: left;
-`;
+const Image = styled('img')(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.down('iPad')]: {
+    maxWidth: '360px',
+  },
+}));
 
-const Description = styled.span<{ isMobile: boolean }>`
-  font-size: ${(props) => (props.isMobile ? '16px' : '18px')};
-  line-height: ${(props) => (props.isMobile ? '22.4px' : '25.2px')};
-  font-weight: 400;
-  text-align: left;
-`;
+const TitleText = styled('span')(({ theme }) => ({
+  fontSize: '54px',
+  lineHeight: '59.4px',
+  fontWeight: '700',
+  textAlign: 'left',
+  [theme.breakpoints.down('tablet')]: {
+    fontSize: '24px',
+    lineHeight: '26.4px',
+  },
+}));
+
+const Description = styled('span')(({ theme }) => ({
+  fontSize: '18px',
+  lineHeight: '25.2px',
+  fontWeight: '400',
+  textAlign: 'left',
+  [theme.breakpoints.down('tablet')]: {
+    fontSize: '16px',
+    lineHeight: '22.4px',
+  },
+}));
