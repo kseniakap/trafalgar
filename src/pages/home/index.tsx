e69cb18aab862b67e80f1e940ec76f4114d2d90a
Alexander Revenko <revenko.a@bk.ru>
2024-06-25 15:27:13 +0700

    feat(TRA-10): подключил футер в home/index

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
 
 import { PageContainer } from '~/components/page';
+import FooterBlock from '~/components/ui/FooterBlock';
 import { HeaderSection } from '~/components/ui/HeaderSection/HeaderSection';
+import SponsorsBlock from '~/components/ui/SponsorsBlock';
 
 const HomePage: FC = () => {
   return (
     <PageContainer>
       <h2>Главная страница</h2>
       <HeaderSection />
+      <SponsorsBlock />
+      <FooterBlock />
     </PageContainer>
   );
 };
