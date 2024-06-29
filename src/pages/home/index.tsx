257f2c1d6397053e7b58b16e80feaf8f210e88d4
Kalabass <sevagavgav@yandex.ru>
2024-06-29 23:35:58 +0300

    chore(TRA-21): Обновил импорта и добавл ассеты

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 
-import AppMenu from '~/components/menu';
 import { PageContainer } from '~/components/page';
 import { About } from '~/components/ui/About/About';
 import { Arcticles } from '~/components/ui/Articles/Articles';
 import FooterBlock from '~/components/ui/FooterBlock/FooterBlock';
 import { HeaderSection } from '~/components/ui/HeaderSection/HeaderSection';
-import SponsorsBlock from '~/components/ui/SponsorsBlock/SponsorsBlock';
 import { QuestionsBlock } from '~/components/ui/QuestionsBlock/QuestionsBlock';
+import SponsorsBlock from '~/components/ui/SponsorsBlock/SponsorsBlock';
+import AppMenu from '~/components/ui/appMenu';
 
 const HomePage: FC = () => {
   return (
@@ -16,7 +16,7 @@ const HomePage: FC = () => {
       <HeaderSection />
       <About />
       <Arcticles />
-      <QuestionsBlock/>
+      <QuestionsBlock />
       <SponsorsBlock />
       <FooterBlock />
     </PageContainer>
